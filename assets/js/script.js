371438b4a8e7f0a911ff2809e35b299648bdba1c
Suman028 <sumans@axioned.com>
2021-11-03 12:57:15 +0530

    filter tabs js done

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,9 @@ $(document).ready(function () {
   var hmenu = $(".menu");
   var nav = $("nav ul");
   var counter_ctn = $(".slider-ctn").offset().top;
+  var filtertabs = $(".filter-tab");
+  var btn = $(".tab");
+  var card = $(".card");
 
   slider.slick({
     fade: true,
@@ -26,33 +29,33 @@ $(document).ready(function () {
     nav.toggleClass("nav-active");
   });
 
-  $(window).scroll(function(){
-      if ($(window).scrollTop() > counter_ctn) {
-        $(".count").each(function () {
-            var $this = $(this),
-              countTo = $this.attr("data-target");
-        
-            $({ countNum: $this.text() }).animate(
-              {
-                countNum: countTo,
-              },
-        
-              {
-                duration: 3000,
-                easing: "linear",
-                step: function () {
-                  $this.text(Math.floor(this.countNum));
-                },
-                complete: function () {
-                  $this.text(this.countNum);
-                },
-              }
-            );
-          });
-      }
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > counter_ctn) {
+      $(".count").each(function () {
+        var $this = $(this),
+          countTo = $this.attr("data-target");
+
+        $({ countNum: $this.text() }).animate(
+          {
+            countNum: countTo,
+          },
+
+          {
+            duration: 3000,
+            easing: "linear",
+            step: function () {
+              $this.text(Math.floor(this.countNum));
+            },
+            complete: function () {
+              $this.text(this.countNum);
+            },
+          }
+        );
+      });
+    }
   });
 
-  $('.tab-btn').click(function(){
+  $('.tab-btn').click(function () {
     $('.feat-active').removeClass('feat-active');
     console.log($(this));
     $(this).children().addClass('feat-active');
@@ -60,5 +63,40 @@ $(document).ready(function () {
     const index = $(this).index();
     console.log(index);
     $('.tabs').eq(index).addClass('tab-active');
+  });
+
+  filtertabs.on('click touch', '.all', function () {
+    btn.removeClass('active-tab');
+    $(this).addClass('active-tab');
+
+    card.hide();
+    card.fadeIn(700);
+  });
+
+  filtertabs.on('click', '.creative', function () {
+    btn.removeClass('active-tab');
+    $(this).addClass('active-tab');
+
+    card.show();
+    card.fadeOut(400);
+    card.filter('.creative').fadeIn(400);
+  });
+
+  filtertabs.on('click', '.corporate', function () {
+    btn.removeClass('active-tab');
+    $(this).addClass('active-tab');
+
+    card.show();
+    card.fadeOut(400);
+    card.filter('.corporate').fadeIn(400);
+  });
+
+  filtertabs.on('click', '.portfolio', function () {
+    btn.removeClass('active-tab');
+    $(this).addClass('active-tab');
+
+    card.show();
+    card.fadeOut(400);
+    card.filter('.portfolio').fadeIn(400);
   });
 });
